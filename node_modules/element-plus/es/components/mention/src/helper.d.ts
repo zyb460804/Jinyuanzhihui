0e52ee14cb44b587c6ae4a68e651cd72c9ca5a33
zyb <16542870+zyb152535@user.noreply.gitee.com>
2026-04-10 21:00:57 +0800

    删除node_modules,写完后端接口，优化页面。

--- node_modules/element-plus/es/components/mention/src/helper.d.ts
+++
@@ -1,6 +0,0 @@
-import { MentionOption } from "./types.js";
-
-//#region ../../packages/components/mention/src/helper.d.ts
-declare const filterOption: (pattern: string, option: MentionOption) => boolean;
-//#endregion
-export { filterOption };
